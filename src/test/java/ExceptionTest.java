87dafbd1da0f220e99741e3b5f6630d0f6759aa6
eucrabs <QpMzH123>
2022-08-29 10:37:22 +0300

    Exception checking

--- src/test/java/ExceptionTest.java
+++ src/test/java/ExceptionTest.java
@@ -1,41 +1,27 @@
 import com.example.Feline;
 import com.example.Lion;
+
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 // Extra test class for checking Exceptions
 public class ExceptionTest extends BaseTest {
 
     // Exception in Lion class
-    @Test(expected = Exception.class)
-    public void catchExceptionWhenLionHasInvalidSex() throws Exception {
-        Lion lion = new Lion("Test", feline);
-    }
-
     @Test
-    public void checkExceptionTextForLion() {
-        try {
-            Lion lion = new Lion("Test", feline);
-        } catch (Exception e) {
-            assertEquals("Используйте допустимые значения пола животного - самец или самка", e.getMessage());
-        }
+    public void catchExceptionWhenLionHasInvalidSex() {
+
+        Exception exception = assertThrows(Exception.class, () -> new Lion("Test", feline));
+        assertEquals("Используйте допустимые значения пола животного - самец или самка", exception.getMessage());
+
     }
 
     // Exception in Feline class
-    @Test(expected = Exception.class)
-    public void catchExceptionWhenFelineHasInvalidAnimalKind() throws Exception {
-        feline = new Feline();
-        feline.getFood("Test");
-    }
-
     @Test
-    public void checkExceptionTextForFeline() {
-        try {
-            feline = new Feline();
-            feline.getFood("Test");
-        } catch (Exception e) {
-            assertEquals("Неизвестный вид животного, используйте значение Травоядное или Хищник", e.getMessage());
-        }
+    public void catchExceptionWhenFelineHasInvalidAnimalKind() {
+        feline = new Feline();
+        Exception exception = assertThrows(Exception.class, () -> feline.getFood("Test"));
+        assertEquals("Неизвестный вид животного, используйте значение Травоядное или Хищник", exception.getMessage());
     }
 }
